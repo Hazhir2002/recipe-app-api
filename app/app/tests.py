6bedd41084650df40ee56819374a3a3d937418f2
Hazhir2002 <HazhirYousefi2002@gmail.com>
2024-10-28 15:31:52 +0330

    linting again

--- app/app/tests.py
+++ app/app/tests.py
@@ -11,13 +11,13 @@ class CalcTests(SimpleTestCase):
     """Test the calc module."""
 
     def test_add_numbers(self):
-      """Test adding numbers together."""
-      res = calc.add(5, 6)
+        """Test adding numbers together."""
+        res = calc.add(5, 6)
 
-      self.assertEqual(res, 11)
+        self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
-      """Test subtracting numbers."""
-      res = calc.subtrac(10, 15)
+        """Test subtracting numbers."""
+        res = calc.subtrac(10, 15)
 
-      self.assertEqual(res, 5)
+        self.assertEqual(res, 5)
