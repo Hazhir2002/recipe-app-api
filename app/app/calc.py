6663c6df382a5dc2fc20e1ec54246f1e4e399b92
Hazhir2002 <HazhirYousefi2002@gmail.com>
2024-10-28 15:21:23 +0330

    fixing Linting

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,12 @@
 Calculator functions
 """
 
+
 def add(x, y):
-  """Add x and y and return result."""
-  return x + y
+    """Add x and y and return result."""
+    return x + y
+
 
 def subtrac(x, y):
-  """Subtrac x and y return result."""
-  return y - x
+    """Subtrac x and y return result."""
+    return y - x
