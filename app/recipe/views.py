e8f0a92c56baf39eb94a468d16bfe41e8085434c
Hazhir2002 <HazhirYousefi2002@gmail.com>
2024-12-11 14:56:10 +0330

    added delete functionality to tags

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -41,6 +41,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
 
 class TagViewSet(
+    mixins.DestroyModelMixin,
     mixins.UpdateModelMixin,
     mixins.ListModelMixin,
     viewsets.GenericViewSet,
